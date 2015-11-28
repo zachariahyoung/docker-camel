e2e450995b2b23e493c3be44985c3b8c50b1a06b
Zachariah Young <zpyoung@gmail.com>
2015-11-28 10:21:11 -0600

    Add exception handling

--- src/main/java/com/zandroid/camel/FirstRoute.java
+++ src/main/java/com/zandroid/camel/FirstRoute.java
@@ -1,5 +1,7 @@
 package com.zandroid.camel;
 
+import org.apache.camel.CamelExchangeException;
+import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.stereotype.Component;
@@ -7,20 +9,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class FirstRoute extends RouteBuilder {
 
-	@Override
-	public void configure() throws Exception {
-		
-			from("timer://trigger")
-				.transform()
-				.simple("ref:myBean")			
-				.to("activemq:queue:TRIGGER");
-		
-			    from("activemq:TRIGGER?transacted=true")
-			    .bean("exceptionTest")
-			    .to("log:out","activemq:queue:TEST");
-			  
-		
-		
-	}
+    @Override
+    public void configure() throws Exception {
+
+        onException(RuntimeCamelException.class)
+                .to("activemq:queue:EXCEPTION");
+
+        from("timer://trigger")
+                .transform()
+                .simple("ref:myBean")
+                .to("activemq:queue:TRIGGER");
+
+        from("activemq:TRIGGER")
+                .bean("exceptionTest")
+                .to("log:out", "activemq:queue:TEST");
+
+
+    }
 
 }
